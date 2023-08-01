ce2ee88db9fc4305c008d0f73c810a6ef88d33b0
Leif.xing <Leif-xing@users.noreply.github.com>
2023-08-01 20:26:57 +0800

    Update start.sh
    加入端口检测，未开放则启动服务

--- ngrok/start.sh
+++ ngrok/start.sh
@@ -1,3 +1,18 @@
-/etc/init.d/ssh restart
-service xrdp restart
+#!/bin/bash
+
+# 检测2222端口是否开放
+netstat -tunl | grep ":2222 " > /dev/null 2>&1
+if [[ $? -ne 0 ]]; then
+    /etc/init.d/ssh restart
+else
+    echo "2222端口已经开放"
+fi
+# 检测13388端口是否开放
+netstat -tunl | grep ":13388 " > /dev/null 2>&1
+if [[ $? -ne 0 ]]; then
+    service xrdp restart
+else
+    echo "13388端口已经开放"
+fi
+
 ngrok start --all --config=tunnels.yml
